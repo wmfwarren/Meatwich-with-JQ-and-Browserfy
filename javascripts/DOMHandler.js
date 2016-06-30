fa341bc9aa92a3b4cbce5903509d42d374442b02
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-30 09:13:31 -0500

    fixed circular browserfy dependency

--- javascripts/DOMHandler.js
+++ javascripts/DOMHandler.js
@@ -2,12 +2,16 @@
 
 var sandwichMaker = require("./main.js");
 
-
+//this function selects the food based on the drop down menu
 var foodPicker = function(breadObj, cheeseObj, meatObj){
+	var foodTypeArray = ["meat", "cheese", "bread"];
 	$(document).on("change", ".itemPicker", function(){
-		// if($(this){
-
-		// }
+		for(let i = 0; i < foodTypeArray.length; i++){
+			if($(this).prop("id") === foodTypeArray[i]){
+				// console.log("this JQ", $(this).prop("id") );
+				
+			}
+		}
 	});
 }
 
