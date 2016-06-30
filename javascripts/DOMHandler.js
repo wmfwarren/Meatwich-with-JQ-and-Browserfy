3a3b156f41fcdad8a736d70240d7bafb02d166d0
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-30 09:35:22 -0500

    prints ingredients to DOM

--- javascripts/DOMHandler.js
+++ javascripts/DOMHandler.js
@@ -3,13 +3,14 @@
 var sandwichMaker = require("./main.js");
 
 //this function selects the food based on the drop down menu
-var foodPicker = function(breadObj, cheeseObj, meatObj){
+var foodPicker = function(bread_Obj, cheese_Obj, meat_Obj){
 	var foodTypeArray = ["meat", "cheese", "bread"];
+	var price = 0;
 	$(document).on("change", ".itemPicker", function(){
 		for(let i = 0; i < foodTypeArray.length; i++){
 			if($(this).prop("id") === foodTypeArray[i]){
-				// console.log("this JQ", $(this).prop("id") );
-				
+				$("#meatwichOutput").append(`<p class="item">${$(this).val()}</p>`);
+				price += 
 			}
 		}
 	});
